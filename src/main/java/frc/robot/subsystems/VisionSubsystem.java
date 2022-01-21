7dba4588599269cdc1b7d642157f4c1f43cf1e0e
sharedcomputer <95010825+sharedcomputer@users.noreply.github.com>
2022-01-21 16:54:16 -0600

    Vision tests

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.awt.Color;
+
 import org.opencv.core.Mat;
 
 import edu.wpi.cscore.CvSink;
@@ -32,19 +34,37 @@ public class VisionSubsystem extends SubsystemBase {
                 continue;
             }
             Mat output = source.clone();
-            // for (int row = 0; row < output.rows(); row++) {
-            //     for (int col = 0; col < output.cols(); col++) {
-            //         double[] pixels = source.get(row, col);
-            //         double blue = pixels[0];
-            //         double green = pixels[1];
-            //         double red = pixels[2];
-            //         if (blue > 150 && (blue - red) > 20 && (blue - green) > 20) {
-            //             output.put(row, col, pixels);
-            //         } else {
-            //             output.put(row, col, 0, 0, 0);
-            //         }
-            //     }
-            // }
+
+            // double[] rgbOut = new double[4];
+            float[] hsvOut = new float[3];
+
+            for (int row = 0; row < output.rows(); row++) {
+                for (int col = 0; col < output.cols(); col++) {
+                    double[] rgbOut = source.get(row, col);
+                    int blue = (int) rgbOut[0];
+                    int green = (int) rgbOut[1];
+                    int red = (int) rgbOut[2];
+
+                    Color.RGBtoHSB(red, green, blue, hsvOut);
+                    float hue = hsvOut[0] * 360;
+                    float saturation = hsvOut[1];
+                    float brightness = hsvOut[2];
+
+                    if (
+                        hue > 190 &&
+                        hue < 240 &&
+                        saturation < 1 &&
+                        brightness < 0.8 &&
+                        brightness > 0.2
+                    ) {
+                        output.put(row, col, blue, green, red);
+                    } else {
+                        output.put(row, col, 0, 0, 0);
+                    }
+                    // output.put(row, col, 255 - blue, 255 - green, 255 - red);
+                }
+            }
+            // Imgproc.invert
             outputStream.putFrame(output);
         }
     }
